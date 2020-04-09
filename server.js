fab80ca486b8fe15926cf5e50d4609496b262814
Bryan Almaraz <bryan.almaraz96@gmail.com>
2020-04-09 22:32:59 +0800

    update server for deployment

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.use('/api/countries', require('./routes/countries'));
 app.use('/api/categories', require('./routes/categories'));
 app.use('/api/expense', require('./routes/expenses'));
 
+// Server static assets in production
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log('server running'));
